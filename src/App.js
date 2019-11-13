db5de0dba2e52e979e5dabe9a46cb8060b438419
Roberto Dote <roberto.dote@globant.com>
2019-11-13 17:07:00 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slider from 'infinite-react-carousel';
+import { slides, categories, recommended } from './mocks' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>
+  <div className="netflux">
+    <div className="netflux__slider">
+      <Slider dots arrows={false} className="netflux__sliderBox">
+        {slides.map(slide =>
+          <div className="netflux__slider__item" key={slide.id}>
+            <img src={slide.url} alt={slide.title} />
+          </div>
+        )}
+      </Slider>
     </div>
-  );
-}
+
+    <div className="netflux__categories">
+      {categories.map(category =>
+        <div className="netflux__categories__item" key={category.id}>
+          <img src={category.thumbnail} alt={category.name} />
+        </div>
+      )}
+    </div>
+    
+    <div className="netflux__recommended">
+      <h2>Recommended For You</h2>
+      <div className="netflux__recommendedBox">
+        {recommended.map(item =>
+          <div className="netflux__recommended__item" key={item.id}>
+            <img src={item.thumbnail} alt={item.title} />
+          </div>
+        )}
+      </div>
+    </div>
+
+    <div className="netflux__watching">
+      <h2>Continue Watching</h2>
+      <div className="netflux__watchingBox">
+        {recommended.map(item =>
+          <div className="netflux__watching__item" key={item.id}>
+            <img src={item.thumbnail} alt={item.title} />
+          </div>
+        )}
+      </div>
+    </div>
+  </div>
 
 export default App;
---
+++ src/mocks.js
@@ -0,0 +1,79 @@
+const slides = [
+  {
+    id: 1,
+    url: 'http://placehold.it/800x400',
+    title: 'one'
+  },
+  {
+    id: 2,
+    url: 'http://placehold.it/800x400',
+    title: 'two'
+  },
+  {
+    id: 3,
+    url: 'http://placehold.it/800x400',
+    title: 'three'
+  }
+]
+
+const categories = [
+  {
+    id: 1,
+    name: 'one',
+    thumbnail: 'http://placehold.it/100x40'
+  },
+  {
+    id: 2,
+    name: 'two',
+    thumbnail: 'http://placehold.it/100x40'
+  },
+  {
+    id: 3,
+    name: 'three',
+    thumbnail: 'http://placehold.it/100x40'
+  },
+  {
+    id: 4,
+    name: 'four',
+    thumbnail: 'http://placehold.it/100x40'
+  },
+  {
+    id: 5,
+    name: 'five',
+    thumbnail: 'http://placehold.it/100x40'
+  }
+]
+
+const recommended = [
+  {
+    id: 1,
+    title: 'one',
+    thumbnail: 'http://placehold.it/220x130'
+  },
+  {
+    id: 2,
+    title: 'two',
+    thumbnail: 'http://placehold.it/220x130'
+  },
+  {
+    id: 3,
+    title: 'three',
+    thumbnail: 'http://placehold.it/220x130'
+  },
+  {
+    id: 4,
+    title: 'four',
+    thumbnail: 'http://placehold.it/220x130'
+  },
+  {
+    id: 5,
+    title: 'five',
+    thumbnail: 'http://placehold.it/220x130'
+  }
+]
+
+export {
+  slides,
+  categories,
+  recommended
+}
